62b4a34871646869fe5288a2001a9cbffa83d2c9
Ronaldo Goncalves <rgoncalves1026@gmail.com>
2021-06-09 14:02:35 +0100

    index, new, show and create methods working

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,44 @@
 class PostsController < ApplicationController
-  skip_before_action :authenticate_user!, only: :index
-  
+  skip_before_action :authenticate_user!, only: [:index, :show, :new, :create]
+
   def index
+    @posts = Post.all
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      redirect_to post_path(@post)
+    else
+      render 'new'
+    end
+  end
+
+  def edit
   
   end
+
+  def update
+  
+  end
+
+  def destroy
+  
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :position, :store_name, :story)
+  end
+
 end
