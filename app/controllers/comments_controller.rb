f7f9657d6cf4bd83ee3185f696297f4332cbc581
Ronaldo Goncalves <rgoncalves1026@gmail.com>
2021-06-10 22:15:01 +0100

    comments added to my project

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,14 @@
 class CommentsController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:create]
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.create(comment_params)
+    redirect_to post_path(@post)
+  end
+
+  private
+  
+  def comment_params
+    params.require(:comment).permit(:body)
+  end
 end
