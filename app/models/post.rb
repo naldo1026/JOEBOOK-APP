672583b92d36c527756878800c809d00a41f5fae
Ronaldo Goncalves <rgoncalves1026@gmail.com>
2021-06-18 13:17:25 +0100

    validations for botm post and comment models

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,9 @@
 class Post < ApplicationRecord
   has_many :comments
   belongs_to :user
+
+  validates :title, presence: true
+  validates :position, presence: true
+  validates :store_name, presence: true
+  validates :story, presence: true
 end
