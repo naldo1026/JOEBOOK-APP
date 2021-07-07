a1eb95ffcbbb8de1618302059df103f514b903be
Ronaldo Goncalves <rgoncalves1026@gmail.com>
2021-07-07 17:09:59 +0100

    new and create and index methods added for covers page

--- app/controllers/covers_controller.rb
+++ app/controllers/covers_controller.rb
@@ -1,2 +1,24 @@
 class CoversController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index, :show, :new, :create, :edit, :update, :destroy]
+  def index
+    @covers = Cover.all
+  end
+
+  def new
+    @cover = Cover.new
+  end
+
+  def create
+    @cover = Cover.create(cover_params)
+
+    if @cover.save!
+      redirect_to covers_path
+    end
+  end
+
+  private
+
+  def cover_params
+    params.require(:cover).permit(:store_name, :date_of_shift, :time_of_shift, :descrption)
+  end
 end
