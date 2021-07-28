f5d9bf2733db900d8fac6b7cc80d9c0bf00c245b
Ronaldo Goncalves <rgoncalves1026@gmail.com>
2021-07-28 11:17:56 +0100

    cover page fully working and some styling added to it

--- app/controllers/covers_controller.rb
+++ app/controllers/covers_controller.rb
@@ -1,7 +1,7 @@
 class CoversController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index, :show, :new, :create, :accept, :edit, :update, :destroy]
   def index
-    @covers = Cover.all
+    @covers = Cover.all.order("created_at DESC")
   end
 
   def show
